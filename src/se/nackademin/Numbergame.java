c0fe3ae3789d82902aa8b6ea750e58fe2c33fd0b
Albin Gudmundsson <albin.gudmundsson@gmail.com>
2019-10-30 09:53:07 +0100

    g-level functionality finished

--- src/se/nackademin/Numbergame.java
+++ src/se/nackademin/Numbergame.java
@@ -2,30 +2,48 @@ package se.nackademin;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.Flow;
 
 public class Numbergame extends JFrame {
 
     static JPanel panel = new JPanel();
+    static JPanel mainPanel = new JPanel();
+    static JPanel topPanel = new JPanel();
+
     static JButton[] buttons = new JButton[Main.size*Main.size];
-    static public int test = 0;
+    static JButton newGame = new JButton("New game");
+    //static JTextField sizeInput = new JTextField("4");
+
 
 
     Numbergame () {
-        setSize(500,500);
+        setSize(500,400);
         setLocationRelativeTo(null);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+        add(mainPanel);
+        mainPanel.setLayout(new BorderLayout());
+        mainPanel.add(panel, BorderLayout.CENTER);
+        mainPanel.add(topPanel, BorderLayout.NORTH);
+        topPanel.add(newGame);
+        topPanel.setLayout(new FlowLayout());
+        //topPanel.add(sizeInput);
 
+        newGame.addActionListener(e -> {
+            randomizeGame(buttons);
+        });
 
-        add(panel);
 
         panel.setLayout(new GridLayout(Main.size,Main.size));
+
 
         for (int i = 0; i < buttons.length;i++) {
             buttons[i] = new JButton();;
             buttons[i].setText(String.valueOf(Main.numbers[i]));
-            buttons[i].setPreferredSize(new Dimension(100,100));
+            buttons[i].setPreferredSize(new Dimension(65,65));
             int finalI = i;
             buttons[i].addActionListener(e -> {
 
@@ -39,7 +57,12 @@ public class Numbergame extends JFrame {
                      }
 
                      buttons[finalI].setText("0");
+                     //todo töm siffror från knappar till numbers array??
                      buttons[finalI].setVisible(false);
+
+                     if (winCheck(buttons))
+                         winPopup();
+
                  }
 
             });
@@ -85,6 +108,57 @@ public class Numbergame extends JFrame {
                 return true;
         }
         return false;
+    }
+
+    public static void numbersSwitch () {
+        // todo fixa funktion som körs i verifymove som byter plats på intarna i listan
+
+    }
+
+    public static void winPopup () {
+        JOptionPane.showMessageDialog(null, "Congrats you won!");
+    }
+
+    public static boolean winCheck (JButton[] btns) {
+        StringBuilder fromButtons = new StringBuilder();
+        StringBuilder winCompare = new StringBuilder();
+
+        for (JButton b:btns) {
+            fromButtons.append(b.getText());
+        }
+
+        for (int i = 1; i < btns.length; i++)
+            winCompare.append(i);
+
+        winCompare.append(0);
+
+        if (fromButtons.compareTo(winCompare) == 0)
+            return true;
+        return false;
+    }
+
+    public static void randomizeGame (JButton[] btns) {
+
+        Random r = new Random();
+
+
+        for (int i = 0; i < btns.length;i++) {
+
+            String temp = btns[i].getText();
+            int tempR = r.nextInt(btns.length);
+
+            btns[i].setText(btns[tempR].getText());
+            btns[tempR].setText(temp);
+
+            for (JButton b:btns) {
+                if (b.getText().equalsIgnoreCase("0"))
+                    b.setVisible(false);
+                else
+                    b.setVisible(true);
+            }
+
+        }
+
     }
 
     public static void main () {
